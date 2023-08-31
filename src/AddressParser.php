84dc2cc6bdacb65b70f0ae234de6fcc6c23ba485
a443939412 <443939412@qq.com>
2023-08-31 18:36:23 +0800

    手机号支持虚拟号匹配；支持国际区号+86形式的匹配

--- src/AddressParser.php
+++ src/AddressParser.php
@@ -235,7 +235,7 @@ class AddressParser
 
                         $mobile = preg_replace('/0-|0?(\d{3})[ -](\d{4})[ -](\d{4})/', '$1$2$3', $mobile);
 
-                        if (preg_match('/(?<!\d)1[0-9]{10}(?!\d)|(?<!\d)(?:\d{3,4}\-)?\d{8}(?:\-\d+)?(?!\d)/U', $mobile, $match)) {
+                        if (preg_match('/(?<!\d|\+)(?:\+?\d{2,}[ -])?1[0-9]{10}(?!\d)(?:-\d+)?|(?<!\d)(?:\d{3,4}\-)?\d{8}(?:\-\d+)?(?!\d)/', $mobile, $match)) {
                             $result['mobile'] = $match[0];
                             unset($this->config['extra']['mobile']);
                         }
